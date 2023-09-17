b62f8e332d4f0a34e187fe5870f4e891c84b3bec
Dambar Sing <programmer.gshi18@gmail.com>
2023-09-17 21:46:32 +0545

    Extending Class

--- class.js
+++ class.js
@@ -1,9 +1,52 @@
-const Product=class{
+const Product=class{ // Class declaration using expression method
     constructor(itemName,price, discount){
         this.itemName = itemName,
         this.price =price, 
         this.discount =discount
     }
+    // Method with no Parenthesis
+    // get property
+    get getprice(){
+        return this.price;
+    }
+    // set new price
+    set setprice(value){
+        return this.price=value;
+    }
+    // function method (with parenthesis)
+    RecentPrice(){
+        return this.price;
+    }
 }
-let getProduct = new Product("Pencil",200,40)
-console.log(getProduct)
+let Product1 = new Product("Pencil",200,40)
+console.log(Product1);
+//accessing pencil price
+console.log(Product1.getprice);
+//setting new price
+console.log(Product1.setprice=500);
+console.log(Product1.getprice);
+/* Accessing price..
+by function method */
+console.log(Product1.RecentPrice())
+
+/* Extending of class */
+class Product2{
+    constructor(ItemName){
+        this.ItemName = ItemName;
+    }
+    get getItemName(){
+        return this.ItemName + " is a Product2";
+    }
+}
+class Furniture extends Product2{  // Extends the Parent Class
+    constructor(ItemName){
+        super(ItemName); // Super Function -> Call the constructor of the parent Class.
+    }
+    get getItemName(){
+        return this.ItemName + " is a Furniture";
+    }
+}
+let apple = new Product2 ('Apple');
+let table = new Furniture("Table");
+console.log(apple.getItemName);
+console.log(table.getItemName);
